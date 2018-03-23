77e67ca0216a3c209b6439621610444a6dd2f702
Keith A. Hanson <hanson2@rose-hulman.edu>
2018-03-23 00:21:58 -0400

    Revert "Merge branch 'business_logic' into 'master'"
    This reverts merge request !3

--- EclipseWorkspace/Dominion/src/Player.java
+++ EclipseWorkspace/Dominion/src/Player.java
@@ -1,33 +1,14 @@
 
 public class Player {
 
-	public int points;// replace with a tallying function eventually
+	public int points;//replace with a tallying function eventually
 	public String name;
-
-	public Player(String name) {
+	
+	public Player(String name){
 		this.points = 0;
 		this.name = name;
-	}
-
-	public boolean drawACard() {
-		return false;
-		// TODO Auto-generated method stub
-	}
-
-	public boolean discardHand() {
-		return false;
-		// TODO Auto-generated method stub
-	}
-
-	public String playCard() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public boolean buy() {
-		// TODO Auto-generated method stub
-		return false;
 		
 	}
+	
 
 }
