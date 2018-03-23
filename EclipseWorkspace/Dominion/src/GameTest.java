f6914e4c48b5c75ea6de46d5a388bf7a697c5e47
Andy Sadler <andrewsadler122@gmail.com>
2018-03-22 22:00:21 -0400

    GameTest: fix formatting errors

--- EclipseWorkspace/Dominion/src/GameTest.java
+++ EclipseWorkspace/Dominion/src/GameTest.java
@@ -7,20 +7,21 @@ public class GameTest {
 	@Test
 	public void testWinnable() {
 		Game g = new Game(2);
-		assertEquals("0&&1",g.endGame());
+		assertEquals("0&&1", g.endGame());
 	}
+
 	@Test
-	public void testWinnableWPoints(){
+	public void testWinnableWPoints() {
 		Game g = new Game(4);
 		g.players[0].points = 5;
 		g.players[1].points = 3;
 		g.players[2].points = 2;
 		g.players[3].points = 1;
-		assertEquals("0",g.endGame());
+		assertEquals("0", g.endGame());
 	}
 
 	@Test
-	public void testWinnableTieWTiebreakers(){
+	public void testWinnableTieWTiebreakers() {
 		Game g = new Game(4);
 		g.endTurn();
 		g.endTurn();
@@ -28,10 +29,11 @@ public class GameTest {
 		g.players[1].points = 5;
 		g.players[2].points = 5;
 		g.players[3].points = 5;
-		assertEquals("2&&3",g.endGame());
+		assertEquals("2&&3", g.endGame());
 	}
+
 	@Test
-	public void testWinnableOneWinnerTiebreaker(){
+	public void testWinnableOneWinnerTiebreaker() {
 		Game g = new Game(4);
 		g.endTurn();
 		g.endTurn();
@@ -39,18 +41,21 @@ public class GameTest {
 		g.players[1].points = 3;
 		g.players[2].points = 3;
 		g.players[3].points = 5;
-		assertEquals("3",g.endGame());
+		assertEquals("3", g.endGame());
 	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testMoreThan4Players(){
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testMoreThan4Players() {
 		Game g = new Game(5);
 	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testLessThan2Players(){
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testLessThan2Players() {
 		Game g = new Game(0);
 	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testLessThan0Players(){
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testLessThan0Players() {
 		Game g = new Game(-1);
 	}
 
