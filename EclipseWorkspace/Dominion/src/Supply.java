5dbec093e37f30499a691fce737a8bc688519e30
donohoc1 <donohoc1@rose-hulman.edu>
2018-03-26 22:26:57 -0400

    Refactored to simplify the code quite a bit. Reduces number of methods by getting base cards all in one. Still needs some work for Kingdom Cards and making the accessor methods more relevant(returning a card popped off the stack instead of returning the whole stack).

--- EclipseWorkspace/Dominion/src/Supply.java
+++ EclipseWorkspace/Dominion/src/Supply.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
 public class Supply {
@@ -12,46 +14,81 @@ public class Supply {
 	Stack<Card> curseSupply;
 	List<Stack<Card>> kingdomCardList;
 	
-	public Supply(){
-		setUpBaseCards();
-	}
+	Map<String, Stack<Card>> supplyPiles;
 	
-	private void setUpBaseCards(){
-		this.copperSupply = new Stack<Card>();
-		this.silverSupply = new Stack<Card>();
-		this.goldSupply = new Stack<Card>();
-		this.estateSupply = new Stack<Card>();
-		this.duchySupply = new Stack<Card>();
-		this.provinceSupply = new Stack<Card>();
-		this.curseSupply = new Stack<Card>();
+	public Supply(){
+		this.supplyPiles = new HashMap<String, Stack<Card>>();	
 		this.kingdomCardList = new ArrayList<Stack<Card>>();
 		
+		this.setUpCopper();
+		this.setUpSilver();
+		this.setUpGold();
+		this.setUpEstate();
+		this.setUpDuchy();
+		this.setUpProvince();
+		this.setUpCurse();
+		
+		for(int i = 0; i < 10; i++){
+			this.kingdomCardList.add(new Stack<Card>());
+			for(int j = 0; j < 10; j++){
+				this.kingdomCardList.get(i).push(new Copper());				
+			}
+		}
+	}
+	
+	private void setUpCopper(){
+		this.supplyPiles.put("COPPER", new Stack<Card>());
+		this.copperSupply = this.supplyPiles.get("COPPER");
 		for(int i = 0; i < 60; i++){
 			this.copperSupply.push(new Copper());
 		}
+	}
+	
+	private void setUpSilver(){
+		this.supplyPiles.put("SILVER", new Stack<Card>());
+		this.silverSupply = this.supplyPiles.get("SILVER");
 		for(int i = 0; i < 40; i++){
 			this.silverSupply.push(new Silver());
 		}
+	}
+	
+	private void setUpGold(){
+		this.supplyPiles.put("GOLD", new Stack<Card>());
+		this.goldSupply = this.supplyPiles.get("GOLD");
 		for(int i = 0; i < 30; i++){
 			this.goldSupply.push(new Gold());
 		}
+	}
+	
+	private void setUpEstate(){
+		this.supplyPiles.put("ESTATE", new Stack<Card>());
+		this.estateSupply = this.supplyPiles.get("ESTATE");
 		for(int i = 0; i < 24; i++){
 			this.estateSupply.push(new Estate());
 		}
+	}
+	
+	private void setUpDuchy(){
+		this.supplyPiles.put("DUCHY", new Stack<Card>());
+		this.duchySupply = this.supplyPiles.get("DUCHY");
 		for(int i = 0; i < 12; i++){
 			this.duchySupply.push(new Duchy());
 		}
+	}
+	
+	private void setUpProvince(){
+		this.supplyPiles.put("PROVINCE", new Stack<Card>());
+		this.provinceSupply = this.supplyPiles.get("PROVINCE");
 		for(int i = 0; i < 12; i++){
 			this.provinceSupply.push(new Province());
 		}
+	}
+	
+	private void setUpCurse(){
+		this.supplyPiles.put("CURSE", new Stack<Card>());
+		this.curseSupply = this.supplyPiles.get("CURSE");
 		for(int i = 0; i < 30; i++){
 			this.curseSupply.push(new Curse());
-		}
-		for(int i = 0; i < 10; i++){
-			this.kingdomCardList.add(new Stack<Card>());
-			for(int j = 0; j < 10; j++){//Change this 10 to whatever the real number is
-				this.kingdomCardList.get(i).push(new Copper());				
-			}
 		}
 	}
 	
@@ -59,37 +96,41 @@ public class Supply {
 	public List<Stack<Card>> getKingdomCardList() {		
 		return this.kingdomCardList;
 	}
+	
+	public Stack<Card> getBaseSupply(String cardName){
+		return this.supplyPiles.get(cardName);		
+	}
 
 	public Stack<Card> getCopperSupply() {
-		return this.copperSupply;
+		return this.supplyPiles.get("COPPER");
 	}
 
 	public Stack<Card> getSilverSupply() {
-		return this.silverSupply;
+		return this.supplyPiles.get("SILVER");
 	}
 
 	public Stack<Card> getGoldSupply() {
-		return this.goldSupply;
+		return this.supplyPiles.get("GOLD");
 	}
 
 	public Stack<Card> getEstateSupply() {
-		return this.estateSupply;
+		return this.supplyPiles.get("ESTATE");
 	}
 
 	public Stack<Card> getDuchySupply() {
-		return this.duchySupply;
+		return this.supplyPiles.get("DUCHY");
 	}
 
 	public Stack<Card> getProvinceSupply() {
-		return this.provinceSupply;
+		return this.supplyPiles.get("PROVINCE");
 	}
 
 	public Stack<Card> getCurseSupply() {
-		return this.curseSupply;
+		return this.supplyPiles.get("CURSE");
 	}
 
 	public boolean isGameOver() {
-		if(this.provinceSupply.isEmpty()){
+		if(this.supplyPiles.get("PROVINCE").isEmpty()){
 			return true;
 		}
 		if(this.isThreePilesGone()){
@@ -106,29 +147,13 @@ public class Supply {
 				numPilesGone++;
 			}
 		}
-		if(this.copperSupply.isEmpty()){
-			numPilesGone++;
-		}
-		if(this.silverSupply.isEmpty()){
-			numPilesGone++;
-		}
-		if(this.goldSupply.isEmpty()){
-			numPilesGone++;
-		}
-		if(this.estateSupply.isEmpty()){
-			numPilesGone++;
-		}
-		if(this.duchySupply.isEmpty()){
-			numPilesGone++;
-		}
-		if(this.curseSupply.isEmpty()){
-			numPilesGone++;
+		for(String cardName : this.supplyPiles.keySet()){
+			if(this.supplyPiles.get(cardName).isEmpty()){
+				numPilesGone++;
+			}
 		}
 		
-		if(numPilesGone >= 3){
-			return true;
-		}
-		return false;
+		return numPilesGone >= 3;
 	}
 
 }
--- EclipseWorkspace/Dominion/src/SupplyPileTest.java
+++ EclipseWorkspace/Dominion/src/SupplyPileTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import java.util.List;
+import java.util.Stack;
 
 import org.junit.Test;
 
@@ -13,20 +14,20 @@ public class SupplyPileTest {
 		
 		assertTrue(kingdomeCardList.size() == 10);
 		
-		assertTrue(supply.getCopperSupply().size() == 60);
-		assertTrue(supply.getSilverSupply().size() == 40);
-		assertTrue(supply.getGoldSupply().size() == 30);
+		assertTrue(supply.getBaseSupply("COPPER").size() == 60);
+		assertTrue(supply.getBaseSupply("SILVER").size() == 40);
+		assertTrue(supply.getBaseSupply("GOLD").size() == 30);
 		
-		assertTrue(supply.getEstateSupply().size() == 24);
-		assertTrue(supply.getDuchySupply().size() == 12);
-		assertTrue(supply.getProvinceSupply().size() == 12);
-		assertTrue(supply.getCurseSupply().size() == 30);
+		assertTrue(supply.getBaseSupply("ESTATE").size() == 24);
+		assertTrue(supply.getBaseSupply("DUCHY").size() == 12);
+		assertTrue(supply.getBaseSupply("PROVINCE").size() == 12);
+		assertTrue(supply.getBaseSupply("CURSE").size() == 30);
 	}
 	
 	@Test
 	public void testCopperValidity(){
 		Supply supply = new Supply();
-		Card top = supply.getCopperSupply().pop();
+		Card top = supply.getBaseSupply("COPPER").pop();
 		
 		assertTrue(top.getType().equals("TREASURE"));
 		assertTrue(top.getCoinsAdded() == 1);
@@ -35,13 +36,13 @@ public class SupplyPileTest {
 		assertTrue(top.getCardsAdded() == 0);
 		assertTrue(top.getVictoryValue() == 0);
 		assertTrue(top.getCost() == 0);
-		assertTrue(supply.getCopperSupply() == supply.getCopperSupply());
+		assertTrue(supply.getBaseSupply("COPPER") == supply.getBaseSupply("COPPER"));
 	}
 	
 	@Test
 	public void testSilverValidity(){
 		Supply supply = new Supply();
-		Card top = supply.getSilverSupply().pop();
+		Card top = supply.getBaseSupply("SILVER").pop();
 		
 		assertTrue(top.getType().equals("TREASURE"));
 		assertTrue(top.getCoinsAdded() == 2);
@@ -50,13 +51,13 @@ public class SupplyPileTest {
 		assertTrue(top.getCardsAdded() == 0);
 		assertTrue(top.getVictoryValue() == 0);
 		assertTrue(top.getCost() == 3);
-		assertTrue(supply.getSilverSupply() == supply.getSilverSupply());
+		assertTrue(supply.getBaseSupply("SILVER") == supply.getBaseSupply("SILVER"));
 	}
 	
 	@Test
 	public void testGoldValidity(){
 		Supply supply = new Supply();
-		Card top = supply.getGoldSupply().pop();
+		Card top = supply.getBaseSupply("GOLD").pop();
 		
 		assertTrue(top.getType().equals("TREASURE"));
 		assertTrue(top.getCoinsAdded() == 3);
@@ -65,13 +66,13 @@ public class SupplyPileTest {
 		assertTrue(top.getCardsAdded() == 0);
 		assertTrue(top.getVictoryValue() == 0);
 		assertTrue(top.getCost() == 6);
-		assertTrue(supply.getGoldSupply() == supply.getGoldSupply());
+		assertTrue(supply.getBaseSupply("GOLD") == supply.getBaseSupply("GOLD"));
 	}
 	
 	@Test
 	public void testEstateValidity(){
 		Supply supply = new Supply();
-		Card top = supply.getEstateSupply().pop();
+		Card top = supply.getBaseSupply("ESTATE").pop();
 		
 		assertTrue(top.getType().equals("VICTORY"));
 		assertTrue(top.getCoinsAdded() == 0);
@@ -80,13 +81,13 @@ public class SupplyPileTest {
 		assertTrue(top.getCardsAdded() == 0);
 		assertTrue(top.getVictoryValue() == 1);
 		assertTrue(top.getCost() == 2);
-		assertTrue(supply.getEstateSupply() == supply.getEstateSupply());
+		assertTrue(supply.getBaseSupply("ESTATE") == supply.getBaseSupply("ESTATE"));
 	}
 	
 	@Test
 	public void testDuchyValidity(){
 		Supply supply = new Supply();
-		Card top = supply.getDuchySupply().pop();
+		Card top = supply.getBaseSupply("DUCHY").pop();
 		
 		assertTrue(top.getType().equals("VICTORY"));
 		assertTrue(top.getCoinsAdded() == 0);
@@ -95,13 +96,13 @@ public class SupplyPileTest {
 		assertTrue(top.getCardsAdded() == 0);
 		assertTrue(top.getVictoryValue() == 3);
 		assertTrue(top.getCost() == 5);
-		assertTrue(supply.getDuchySupply() == supply.getDuchySupply());
+		assertTrue(supply.getBaseSupply("DUCHY") == supply.getBaseSupply("DUCHY"));
 	}
 	
 	@Test
 	public void testProvinceValidity(){
 		Supply supply = new Supply();
-		Card top = supply.getProvinceSupply().pop();
+		Card top = supply.getBaseSupply("PROVINCE").pop();
 		
 		assertTrue(top.getType().equals("VICTORY"));
 		assertTrue(top.getCoinsAdded() == 0);
@@ -110,13 +111,13 @@ public class SupplyPileTest {
 		assertTrue(top.getCardsAdded() == 0);
 		assertTrue(top.getVictoryValue() == 6);
 		assertTrue(top.getCost() == 8);
-		assertTrue(supply.getProvinceSupply() == supply.getProvinceSupply());
+		assertTrue(supply.getBaseSupply("PROVINCE") == supply.getBaseSupply("PROVINCE"));
 	}
 	
 	@Test
 	public void testCurseValidity(){
 		Supply supply = new Supply();
-		Card top = supply.getCurseSupply().pop();
+		Card top = supply.getBaseSupply("CURSE").pop();
 		
 		assertTrue(top.getType().equals("CURSE"));
 		assertTrue(top.getCoinsAdded() == 0);
@@ -125,15 +126,16 @@ public class SupplyPileTest {
 		assertTrue(top.getCardsAdded() == 0);
 		assertTrue(top.getVictoryValue() == -1);
 		assertTrue(top.getCost() == 0);
-		assertTrue(supply.getCurseSupply() == supply.getCurseSupply());
+		assertTrue(supply.getBaseSupply("CURSE") == supply.getBaseSupply("CURSE"));
 	}
 	
 	@Test
 	public void testGameOverProvinceGone(){
 		Supply supply = new Supply();
+		Stack<Card> provinceSupply = supply.getBaseSupply("PROVINCE");
 		
 		for(int i = 0; i < 12; i++){
-			supply.getProvinceSupply().pop();
+			provinceSupply.pop();
 		}
 		
 		assertTrue(supply.isGameOver());
@@ -148,105 +150,133 @@ public class SupplyPileTest {
 	@Test
 	public void testGameOverOneBaseSupplyGoneNotProvince(){
 		Supply supply = new Supply();
+		Stack<Card> duchySupply = supply.getBaseSupply("DUCHY");
+		
 		for(int i = 0; i < 12; i++){
-			supply.getDuchySupply().pop();
+			duchySupply.pop();
 		}
+		
 		assertFalse(supply.isGameOver());
 	}
 	
 	@Test
 	public void testGameOverThreeBaseSupplyGoneNotProvince(){
 		Supply supply = new Supply();
+		Stack<Card> duchySupply = supply.getBaseSupply("DUCHY");
+		Stack<Card> goldSupply = supply.getBaseSupply("GOLD");
+		Stack<Card> copperSupply = supply.getBaseSupply("COPPER");
+		
 		for(int i = 0; i < 12; i++){
-			supply.getDuchySupply().pop();
+			duchySupply.pop();
 		}
 		for(int i = 0; i < 30; i++){
-			supply.getGoldSupply().pop();
+			goldSupply.pop();
 		}
 		for(int i = 0; i < 60; i++){
-			supply.getCopperSupply().pop();
+			copperSupply.pop();
 		}
+		
 		assertTrue(supply.isGameOver());
 	}
 	
 	@Test 
 	public void testGameOverMoreThanThreeBaseSupplyGoneNotProvince(){
 		Supply supply = new Supply();
+		Stack<Card> duchySupply = supply.getBaseSupply("DUCHY");
+		Stack<Card> goldSupply = supply.getBaseSupply("GOLD");
+		Stack<Card> copperSupply = supply.getBaseSupply("COPPER");
+		Stack<Card> estateSupply = supply.getBaseSupply("ESTATE");
+		
 		for(int i = 0; i < 12; i++){
-			supply.getDuchySupply().pop();
+			duchySupply.pop();
 		}
 		for(int i = 0; i < 30; i++){
-			supply.getGoldSupply().pop();
+			goldSupply.pop();
 		}
 		for(int i = 0; i < 60; i++){
-			supply.getCopperSupply().pop();
+			copperSupply.pop();
 		}
 		for(int i = 0; i < 24; i++){
-			supply.getEstateSupply().pop();
+			estateSupply.pop();
 		}
+		
 		assertTrue(supply.isGameOver());
 	}
 	
 	@Test
 	public void testGameOverThreeKingdomSupplyGone(){
 		Supply supply = new Supply();
+		
 		for(int i = 0; i < 3; i++){
 			for(int j = 0; j < 10; j++){
 				supply.getKingdomCardList().get(i).pop();
 			}
 		}
+		
 		assertTrue(supply.isGameOver());
 	}
 	
 	@Test
 	public void testGameOverFourKingdomSupplyGone(){
 		Supply supply = new Supply();
+		
 		for(int i = 0; i < 4; i++){
 			for(int j = 0; j < 10; j++){
 				supply.getKingdomCardList().get(i).pop();
 			}
 		}
+		
 		assertTrue(supply.isGameOver());
 	}
 	
 	@Test
 	public void testGameOverOneKingdomSupplyGone(){
 		Supply supply = new Supply();
+		
 		for(int j = 0; j < 10; j++){
 			supply.getKingdomCardList().get(0).pop();
 		}
+		
 		assertFalse(supply.isGameOver());
 	}
 	
 	@Test
 	public void testGameOverMixedThreeSupplyGone(){
 		Supply supply = new Supply();
+		Stack<Card> duchySupply = supply.getBaseSupply("DUCHY");
+		Stack<Card> goldSupply = supply.getBaseSupply("GOLD");
+		
 		for(int j = 0; j < 10; j++){
 			supply.getKingdomCardList().get(0).pop();
 		}
 		for(int i = 0; i < 12; i++){
-			supply.getDuchySupply().pop();
+			duchySupply.pop();
 		}
 		for(int i = 0; i < 30; i++){
-			supply.getGoldSupply().pop();
+			goldSupply.pop();
 		}
+		
 		assertTrue(supply.isGameOver());
 	}
 	
 	@Test
 	public void testGameOverMixedFourSupplyGone(){
 		Supply supply = new Supply();
+		Stack<Card> duchySupply = supply.getBaseSupply("DUCHY");
+		Stack<Card> goldSupply = supply.getBaseSupply("GOLD");
+		
 		for(int i = 0; i < 2; i++){
 			for(int j = 0; j < 10; j++){
 				supply.getKingdomCardList().get(i).pop();
 			}
 		}
 		for(int i = 0; i < 12; i++){
-			supply.getDuchySupply().pop();
+			duchySupply.pop();
 		}
 		for(int i = 0; i < 30; i++){
-			supply.getGoldSupply().pop();
+			goldSupply.pop();
 		}
+		
 		assertTrue(supply.isGameOver());
 	}
 }
