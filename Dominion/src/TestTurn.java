3fc00e88257473a42a51ff24030b04af81d92324
donohoc1 <donohoc1@rose-hulman.edu>
2018-03-31 15:51:13 -0400

    Fixed one outdated test that was failing

--- Dominion/src/TestTurn.java
+++ Dominion/src/TestTurn.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.easymock.EasyMock;
 import org.junit.Test;
 
@@ -16,19 +19,29 @@ public class TestTurn {
 	@Test
 	public void testFinalState() {
 		Player player = EasyMock.mock(Player.class);
-
+		Card card = EasyMock.mock(Card.class);
 		Turn turn = new Turn(player);
 
-		EasyMock.expect(player.playCard()).andReturn(new Copper());
+		EasyMock.expect(player.playCard()).andReturn(card);
+		EasyMock.expect(card.getActionsAdded()).andReturn(0);
+		EasyMock.expect(card.getBuysAdded()).andReturn(0);
+		EasyMock.expect(card.getCoinsAdded()).andReturn(0);
+		EasyMock.expect(card.getType()).andReturn(this.getCardTypeSet(CardType.ACTION));
 		EasyMock.expect(player.buy()).andReturn(true);
 		player.discardHand();
 		player.drawNewHand();
 
-		EasyMock.replay(player);
+		EasyMock.replay(player, card);
 
 		turn.run();
 
 		assertEquals(TurnCleanupState.class, turn.getCurrentStateType());
+	}
+	
+	private Set<CardType> getCardTypeSet(CardType type){
+		Set<CardType> toReturn = new TreeSet<CardType>();
+		toReturn.add(type);
+		return toReturn;
 	}
 
 }
