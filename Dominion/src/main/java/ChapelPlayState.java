b30e0c63f43b3ef15ac0b48dde95c8b2a742a2f7
donohoc1 <donohoc1@rose-hulman.edu>
2018-05-13 15:19:33 -0400

    Fully tested the Chapel Play State. Ran PIT tests too.
    Had to add new GUI interaction method to Player called
    chooseCardFromHand which allows them to pick a card from their hand.
    Will be incredibly similar to play card, but not used for playing
    purposes, but other action card purposes.

--- Dominion/src/main/java/ChapelPlayState.java
+++ Dominion/src/main/java/ChapelPlayState.java
@@ -1,4 +1,18 @@
 
 public class ChapelPlayState extends CardPlayState {
 
+	@Override
+	public void run(Turn turn) {
+		boolean response;
+		for (int i = 0; i < 4; i++) {
+			response = turn.player.promptYesNo("Would you like to trash a card from your hand?");
+			if (response) {
+				Card toTrash = turn.player.chooseCardFromHand();
+				turn.player.trashCardFromHand(toTrash.getClass());
+			} else {
+				break;
+			}
+		}
+	}
+
 }
