ec7c1c3d6fcfc3c325e9953fdf3943529b80565a
donohoc1 <donohoc1@rose-hulman.edu>
2018-05-13 14:40:28 -0400

    Fully tested the play state with all possible options.
    Ran PIT and no bugs stayed alive on this code.

--- Dominion/src/main/java/VassalPlayState.java
+++ Dominion/src/main/java/VassalPlayState.java
@@ -1,4 +1,27 @@
+import java.util.Optional;
 
 public class VassalPlayState extends CardPlayState {
+
+	@Override
+	public void run(Turn turn) {
+		Optional<Card> cardPossibility = turn.player.discardTopCardOfDrawPile();
+
+		if (!cardPossibility.isPresent()) {
+			return;
+		} else {
+			Card card = cardPossibility.get();
+			if (card.getType().contains(CardType.ACTION)) {
+				boolean response = turn.player.promptYesNo("Would you like to play this action card?");
+				if (response) {
+					turn.state = card.getPlayState();
+					turn.run();
+				} else {
+					return;
+				}
+			} else {
+				return;
+			}
+		}
+	}
 
 }
--- Dominion/src/test/java/VassalPlayStateTest.java
+++ Dominion/src/test/java/VassalPlayStateTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.Optional;
+
 import org.easymock.EasyMock;
 import org.junit.Test;
 
@@ -10,9 +12,79 @@ public class VassalPlayStateTest {
 		VassalPlayState state = new VassalPlayState();
 		Player player = EasyMock.mock(Player.class);
 		Turn turn = EasyMock.mock(Turn.class);
-		
-				
-		
+
+		turn.player = player;
+
+		EasyMock.expect(player.discardTopCardOfDrawPile()).andReturn(Optional.of(new Copper()));
+
+		EasyMock.replay(player, turn);
+
+		state.run(turn);
+
+		EasyMock.verify(player, turn);
+	}
+
+	@Test
+	public void testPlayStateTopCardNotAvailable() {
+		VassalPlayState state = new VassalPlayState();
+		Player player = EasyMock.mock(Player.class);
+		Turn turn = EasyMock.mock(Turn.class);
+
+		turn.player = player;
+
+		EasyMock.expect(player.discardTopCardOfDrawPile()).andReturn(Optional.empty());
+
+		EasyMock.replay(player, turn);
+
+		state.run(turn);
+
+		EasyMock.verify(player, turn);
+	}
+
+	@Test
+	public void testPlayStateTopCardActionChooseNotPlay() {
+		VassalPlayState state = new VassalPlayState();
+		Card cardToPickUp = new Village();
+		Player player = EasyMock.mock(Player.class);
+		Turn turn = EasyMock.mock(Turn.class);
+
+		turn.player = player;
+		turn.state = state;
+
+		EasyMock.expect(player.discardTopCardOfDrawPile()).andReturn(Optional.of(cardToPickUp));
+		EasyMock.expect(player.promptYesNo("Would you like to play this action card?")).andReturn(false);
+
+		EasyMock.replay(player, turn);
+
+		state.run(turn);
+
+		assertFalse(turn.state.getClass() == cardToPickUp.getPlayState().getClass());
+
+		EasyMock.verify(player, turn);
+	}
+
+	@Test
+	public void testPlayStateTopCardActionChoosePlay() {
+		VassalPlayState state = new VassalPlayState();
+		Card cardToPickUp = new Village();
+		Player player = EasyMock.mock(Player.class);
+		Turn turn = EasyMock.mock(Turn.class);
+
+		turn.player = player;
+		turn.state = state;
+
+		EasyMock.expect(player.discardTopCardOfDrawPile()).andReturn(Optional.of(cardToPickUp));
+		EasyMock.expect(player.promptYesNo("Would you like to play this action card?")).andReturn(true);
+		turn.run();
+		EasyMock.expectLastCall();
+
+		EasyMock.replay(player, turn);
+
+		state.run(turn);
+
+		assertEquals(turn.state.getClass(), cardToPickUp.getPlayState().getClass());
+
+		EasyMock.verify(player, turn);
 	}
 
 }
