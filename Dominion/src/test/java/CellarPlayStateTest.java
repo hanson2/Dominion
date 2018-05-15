5799bf0c4352bd93176e058ea3d358e2c20baec7
donohoc1 <donohoc1@rose-hulman.edu>
2018-05-14 22:46:18 -0400

    Finished the Cellar. Ran PIT and didn't add any new mutants

--- Dominion/src/test/java/CellarPlayStateTest.java
+++ Dominion/src/test/java/CellarPlayStateTest.java
@@ -41,7 +41,7 @@ public class CellarPlayStateTest {
 	}
 	
 	@Test
-	public void testDiscardOneCard() {
+	public void testDiscardLastCard() {
 		CellarPlayState state = new CellarPlayState();
 		Player player = EasyMock.mock(Player.class);
 		Turn turn = EasyMock.mock(Turn.class);
@@ -64,6 +64,100 @@ public class CellarPlayStateTest {
 		state.run(turn);
 		
 		EasyMock.verify(player, turn);
+	}
+	
+	@Test
+	public void testDiscardOneCardNotLast() {
+		CellarPlayState state = new CellarPlayState();
+		Player player = EasyMock.mock(Player.class);
+		Turn turn = EasyMock.mock(Turn.class);
+		Card card = EasyMock.mock(Card.class);
+		turn.player = player;
+		turn.state = state;
+		
+		EasyMock.expect(player.sizeOfHand()).andReturn(2);
+		EasyMock.expect(player.promptYesNo("Would you like to discard a card from your hand for +1 Card?")).andReturn(true);
+		EasyMock.expect(player.chooseCardFromHand()).andReturn(card);
+		EasyMock.expect(player.discardCardFromHand(card.getClass())).andReturn(true);
+		
+		EasyMock.expect(player.sizeOfHand()).andReturn(1);
+		EasyMock.expect(player.promptYesNo("Would you like to discard a card from your hand for +1 Card?")).andReturn(false);
+		
+		player.drawACard();
+		EasyMock.expectLastCall();
+		
+		EasyMock.replay(player, turn);
+		
+		state.run(turn);
+		
+		EasyMock.verify(player, turn);
+	}
+	
+	@Test
+	public void testDiscardMultipleCardsLast() {
+		CellarPlayState state = new CellarPlayState();
+		Player player = EasyMock.mock(Player.class);
+		Turn turn = EasyMock.mock(Turn.class);
+		Card card = EasyMock.mock(Card.class);
+		turn.player = player;
+		turn.state = state;
+		
+		EasyMock.expect(player.sizeOfHand()).andReturn(2);
+		EasyMock.expect(player.promptYesNo("Would you like to discard a card from your hand for +1 Card?")).andReturn(true);
+		EasyMock.expect(player.chooseCardFromHand()).andReturn(card);
+		EasyMock.expect(player.discardCardFromHand(card.getClass())).andReturn(true);
+		
+		EasyMock.expect(player.sizeOfHand()).andReturn(1);
+		EasyMock.expect(player.promptYesNo("Would you like to discard a card from your hand for +1 Card?")).andReturn(true);
+		EasyMock.expect(player.chooseCardFromHand()).andReturn(card);
+		EasyMock.expect(player.discardCardFromHand(card.getClass())).andReturn(true);
+		
+		EasyMock.expect(player.sizeOfHand()).andReturn(0);
+		
+		player.drawACard();
+		EasyMock.expectLastCall();
+		player.drawACard();
+		EasyMock.expectLastCall();
+		
+		EasyMock.replay(player, turn);
+		
+		state.run(turn);
+		
+		EasyMock.verify(player, turn);		
+	}
+	
+	@Test
+	public void testDiscardMultipleCardsNotLast() {
+		CellarPlayState state = new CellarPlayState();
+		Player player = EasyMock.mock(Player.class);
+		Turn turn = EasyMock.mock(Turn.class);
+		Card card = EasyMock.mock(Card.class);
+		turn.player = player;
+		turn.state = state;
+		
+		EasyMock.expect(player.sizeOfHand()).andReturn(5);
+		EasyMock.expect(player.promptYesNo("Would you like to discard a card from your hand for +1 Card?")).andReturn(true);
+		EasyMock.expect(player.chooseCardFromHand()).andReturn(card);
+		EasyMock.expect(player.discardCardFromHand(card.getClass())).andReturn(true);
+		
+		EasyMock.expect(player.sizeOfHand()).andReturn(4);
+		EasyMock.expect(player.promptYesNo("Would you like to discard a card from your hand for +1 Card?")).andReturn(true);
+		EasyMock.expect(player.chooseCardFromHand()).andReturn(card);
+		EasyMock.expect(player.discardCardFromHand(card.getClass())).andReturn(true);
+		
+		EasyMock.expect(player.sizeOfHand()).andReturn(3);
+		EasyMock.expect(player.promptYesNo("Would you like to discard a card from your hand for +1 Card?")).andReturn(false);
+		
+		player.drawACard();
+		EasyMock.expectLastCall();
+		player.drawACard();
+		EasyMock.expectLastCall();
+		
+		EasyMock.replay(player, turn);
+		
+		state.run(turn);
+		
+		EasyMock.verify(player, turn);		
 	}
 
 }
