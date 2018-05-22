196ad8309e412d7e587b0ec9e37479c97d46674b
Keith Hanson <hanson2@rose-hulman.edu>
2018-05-22 02:14:35 -0400

    added Bandit without playstate

---
+++ Dominion/src/main/java/cards/Bandit.java
@@ -0,0 +1,33 @@
+package cards;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import util.CardType;
+import util.GameConstants;
+
+public class Bandit extends Card {
+
+	@Override
+	public Set<CardType> getType() {
+		Set<CardType> types = new TreeSet<CardType>();
+		types.add(CardType.ACTION);
+		types.add(CardType.ATTACK);
+		return types;
+	}
+
+	@Override
+	public int getCost() {
+		return GameConstants.BANDITCOST;
+	}
+
+	@Override
+	public String getName() {
+		return GameConstants.BANDITNAME;
+	}
+
+	public String getText() {
+		return GameConstants.BANDITTEXT;
+	}
+
+}
